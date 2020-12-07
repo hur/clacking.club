f175223987dc76085534af53d3064d8c53b57838
Atte Niemi <4998544+hur@users.noreply.github.com>
2020-12-07 14:32:39 +0000

    Implement basic responsiveness

--- src/components/TypingComponent.js
+++ src/components/TypingComponent.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import {currentTime, timeDelta} from "../utils/time.js";
 import generateWords from "../utils/words.js";
+import withSizes from 'react-sizes';
 
-export default class TypingComponent extends React.Component {
+const mapSizesToProps = ({ width }) => ({
+    window: width < 600 ? 10: 20,
+  })
+
+ class TypingComponent extends React.Component {
     constructor(props) {
         super(props)
         this.state = {};
+        
 
         // How many characters to display to either side of the current char
-        this.window = 20;
         // Decimal precision for accuracy
         this.precision = 1;
         
         // Text that has been correctly typed
         this.previousText = "";
         // Upcoming text
-        this.nextText = generateWords(this.window * 2);
+        this.nextText = generateWords(this.props.window * 2);
         // The current character to be typed
         this.currentChar = this.nextText.charAt(0);
         this.nextText = this.nextText.slice(1);
@@ -33,9 +38,6 @@ export default class TypingComponent extends React.Component {
         this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
-
-
-
     handleKeyDown(event) {
         if (!this.startTime) {
             this.startTime = currentTime();
@@ -48,8 +50,8 @@ export default class TypingComponent extends React.Component {
             this.previousText += this.currentChar;
             this.currentChar = this.nextText.charAt(0);
             this.nextText = this.nextText.slice(1);
-            if (this.nextText.split(" ").length <= this.window) {
-                this.nextText += ' ' + generateWords(this.window);
+            if (this.nextText.split(" ").length <= this.props.window) {
+                this.nextText += ' ' + generateWords(this.props.window);
             }
         } else {
             if (event.key === "Backspace" || event.key === "Delete") {
@@ -76,13 +78,14 @@ export default class TypingComponent extends React.Component {
     render () {
         return (
             <div className="typing">
-                <span id="previousText">{this.previousText.substring(this.previousText.length-(Math.max(this.window - this.errorText.length, 0)))
-                    .padStart((Math.max(this.window - this.errorText.length, 0)))}</span>
-                <span id="errorText">{this.errorText.slice(-this.window)}</span>
+                <span id="previousText">{this.previousText.substring(this.previousText.length-(Math.max(this.props.window - this.errorText.length, 0)))
+                    .padStart((Math.max(this.props.window - this.errorText.length, 0)))}</span>
+                <span id="errorText">{this.errorText.slice(-this.props.window)}</span>
                 <span id="currentChar">{this.currentChar}</span>
-                <span id="nextText">{this.nextText.substring(0, this.window)}</span>
+                <span id="nextText">{this.nextText.substring(0, this.props.window)}</span>
                 <div id="stats"><span id="wpm">{this.wpm.toString().padStart(3) || 0} words/minute</span><span id="accuracy">{this.accuracy} %</span></div>
             </div>
         );
     }
 }
+export default withSizes(mapSizesToProps)(TypingComponent);
